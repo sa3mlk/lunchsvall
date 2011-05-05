128fce0af42366874557cbe36040697578b4c724
Jonas Gulle <jonas.gulle@gmail.com>
2011-05-05 23:56:52 +0200

    Added new scraper for Invito

---
+++ scrapers/invito.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from BeautifulSoup import BeautifulSoup, NavigableString
+from urllib2 import urlopen
+from datetime import date
+
+URL = "http://www.invitobar.se/sundsvall/index.php/lunch"
+
+def get_daily_specials():
+	page = urlopen(URL)
+	soup = BeautifulSoup(page)
+
+	daily_specials = {"name": "Invito", "specials": []}
+
+	day = date.today().weekday()
+
+	# Only Monday - Friday
+	if day > 4:
+		return daily_specials
+
+	day = [u"MÅNDAG:", u"TISDAG:", u"ONSDAG:", u"TORSDAG:", u"FREDAG:"][day]
+
+	day = soup.find("h3", text=day)
+	parent = day.findParent("tr")
+	td = parent.findChild("td", valign="top")
+
+	# Filter out the strings only
+	only_strings = filter(lambda x: isinstance(x, NavigableString), td.contents)
+	daily_specials["specials"] = [str(c) for c in only_strings]
+
+	return daily_specials
+
+def main():
+	d = get_daily_specials()
+	print d["name"]
+	for c in d["specials"]:
+		print "  ", c
+
+if __name__ == "__main__":
+	main()
+
