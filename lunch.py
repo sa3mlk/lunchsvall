46ad9f5fb3a1f72f140b202d6f4cda5dd89b5061
Jonas Gulle <jonas.gulle@gmail.com>
2011-05-09 22:43:48 +0200

    Added simple caching

--- lunch.py
+++ lunch.py
@@ -1,18 +1,39 @@
 #!/usr/bin/env python
-from wsgiref.util import setup_testing_defaults
+# -*- encoding: utf-8 -*-
+
 from wsgiref.simple_server import make_server
+from datetime import date
+import os.path
 import simplejson as json
 import scrapers
 
 PORT = 8000
 
 def lunchsvall_app(environment, start_response):
+	d = date.today()
+	cached_file = "cache/%d-%02d-%02d.json" % (d.year, d.month, d.day)
+
+	ret = ""
+	if os.path.isfile(cached_file):
+		print "Returning cached file %s" % cached_file
+		with file(cached_file) as f:
+			ret = f.read()
+	else:
+		print "Scraping new day"
+		daily_specials = scrapers.get_daily_specials()
+		with file(cached_file, "w") as f:
+			print "Saving cache as %s" % cached_file
+			f.write(json.dumps(daily_specials))
+		ret = json.dumps(daily_specials)
+
 	status = "200 OK"
-	headers = [("Content-type", "application/json")]
+	headers = [
+		("content-type", "text/plain"),
+		("content-length", str(len(ret)))
+	]
 
 	start_response(status, headers)
-	daily_specials = scrapers.get_daily_specials()
-	return json.dumps(daily_specials)
+	return [ret]
 
 httpd = make_server("", PORT, lunchsvall_app)
 print "Serving on port %d..." % PORT
