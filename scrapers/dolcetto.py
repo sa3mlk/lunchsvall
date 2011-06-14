e6a21a17a3141af89aec34265c562adb2f9ec35a
Jonas Gulle <jonas.gulle@gmail.com>
2011-06-15 00:56:50 +0200

    Removed unused import

--- scrapers/dolcetto.py
+++ scrapers/dolcetto.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding: utf8 -*-
 
-from BeautifulSoup import BeautifulSoup, NavigableString
+from BeautifulSoup import BeautifulSoup
 from urllib2 import urlopen
 from datetime import date
 import re
