ab94cf8ad4b9e780cd92ba8fca5bee2d2ba8b173
Jonas Gulle <jonas.gulle@gmail.com>
2011-07-12 00:07:53 +0200

    Bugfix when they use the same span-tag for all weekdays

--- scrapers/olearys.py
+++ scrapers/olearys.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding: utf8 -*-
 
-from BeautifulSoup import BeautifulSoup
+from BeautifulSoup import BeautifulSoup, Tag, NavigableString
 from urllib2 import urlopen
 from datetime import date
 import re
@@ -32,13 +32,35 @@ def get_daily_specials(day=None):
 	menu = soup.find("span", {"style": style})
 	days = menu.findAll("span")
 
-	def split_specials(text):
-		for i, c in enumerate(text[:-1]):
-			c2 = text[i+1]
-			if c.islower() and c2.isupper() and not c2.isspace():
-				return map(lambda x: x.strip(), [text[0:i+1], text[i+1:]])
+	# Some weeks there are one <span> for each day in the week, but other weeks
+	# they use the same <span> for all weekdays.
 
-	daily_specials["specials"] = split_specials(days[day].text)
+	if days:
+		# They have separate <span>'s for each day
+		def split_specials(text):
+			for i, c in enumerate(text[:-1]):
+				c2 = text[i+1]
+				if c.islower() and c2.isupper() and not c2.isspace():
+					return map(lambda x: x.strip(), [text[0:i+1], text[i+1:]])
+
+		daily_specials["specials"] = split_specials(days[day].text)
+	else:
+		# All days are problably in the same <span>
+		days = filter(lambda x: len(x.text) > 1 if isinstance(x, Tag) else len(x) > 1, menu.contents)
+		days = map(lambda y: unicode(y).strip() if isinstance(y, NavigableString) else y.text.strip(), days)
+		swe_days = [u"Måndag", u"Tisdag", u"Onsdag", u"Torsdag", u"Fredag"]
+		day = swe_days[day]
+		for i, c in enumerate(days):
+			if c == day:
+				# Allow three daily specials
+				for j in range(1, 4):
+					try:
+						if days[i+j] in swe_days:
+							return daily_specials
+						else:
+							daily_specials["specials"].append(days[i+j])
+					except IndexError:
+						return daily_specials
 
 	return daily_specials
 
