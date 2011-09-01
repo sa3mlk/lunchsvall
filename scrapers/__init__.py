0391ef400d9890dc4e93951bc806f730a2021693
Jonas Gulle <jonas.gulle@gmail.com>
2011-09-01 11:00:39 +0200

    Skip scraper results that fail the json transformation

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -19,6 +19,7 @@ import brandstation
 import skonerten
 
 import sys, traceback
+import simplejson as json
 
 def get_daily_specials():
 	scrapers = [
@@ -48,6 +49,9 @@ def get_daily_specials():
 		# The result can either be a list of dicts or a simple dict
 		try:
 			result = scraper.get_daily_specials()
+			# Try to convert so json for sanity reasons, if it fails then skip
+			# that scraper result.
+			json.dumps(result)
 		except Exception, e:
 			traceback.print_exc(file=sys.stderr)
 			print >> sys.stderr, "*"*80
