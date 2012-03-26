f1871110a1757fb3e2fbfe48e3e789105ce266ab
Jonas Gulle <jonas.gulle@gmail.com>
2012-03-26 20:56:43 +0200

    Added Wretmans City too, closing issue #20

--- scrapers/wretmans.py
+++ scrapers/wretmans.py
@@ -6,20 +6,10 @@ from urllib2 import urlopen
 from datetime import date
 import re
 
-URL = "http://wretmans.se/Lunchmenyer/Lunchmeny.htm"
-
-def get_daily_specials(day=None):
-	page = urlopen(URL)
+def scrape_wretmans(url, day=None):
+	page = urlopen(url)
 	soup = BeautifulSoup(page)
 	page.close()
-
-	daily_specials = {
-		"name": "Wretmans",
-		"specials": [],
-		"streetaddress": "Murarvägen 8, Sundsvall",
-		"dataurl": URL,
-		"mapurl": "http://www.hitta.se/ViewDetailsPink.aspx?Vkiid=hvjaJ0xiNoaD1S2mBsxIoA%253d%253d"
-	}
 
 	if day == None:
 		day = date.today().weekday()
@@ -34,22 +24,49 @@ def get_daily_specials(day=None):
 
 	day = [u"Måndag", u"Tisdag", u"Onsdag", u"Torsdag", u"Fredag"][day]
 	anchor = soup.find(lambda tag: tag.name == "p" and re.match(tag.text, day))
-	specials = anchor.findNextSiblings("p", limit=2)
-	for i, s in enumerate([s.text for s in specials]):
+	siblings = anchor.findNextSiblings("p", limit=2)
+	specials = []
+	for i, s in enumerate([s.text for s in siblings]):
 		s = re.sub("\n", " ", s)
 		if s[0:6] == "&nbsp;":
-			daily_specials["specials"].append(s[6:])
+			specials.append(s[6:])
 		else:
-			daily_specials["specials"].append(s)
+			specials.append(s)
+
+	return filter(len, specials)
+
+def get_daily_specials(day=None):
+	urls = [
+		"http://wretmans.se/Lunchmenyer/Lunchmeny.htm",
+		"http://www.wretmans.se/Lunchmenyer/Lunchmeny%20city.htm"
+	]
+
+	daily_specials = [
+		{
+			"name": "Wretmans",
+			"specials": scrape_wretmans(urls[0], day),
+			"streetaddress": "Murarvägen 8, Sundsvall",
+			"dataurl": urls[0],
+			"mapurl": "http://www.hitta.se/ViewDetailsPink.aspx?Vkiid=hvjaJ0xiNoaD1S2mBsxIoA%253d%253d"
+		},
+		{
+			"name": "Wretmans City",
+			"specials": scrape_wretmans(urls[1], day),
+			"streetaddress": "Thulegatan 6, Sundsvall",
+			"dataurl": urls[1],
+			"mapurl": "http://www.hitta.se/wretmans+caf%C3%A9/sundsvall/hzWRPCw5X4"
+		}
+	]
 
 	return daily_specials
 
 def main():
 	for day in range(5):
-		d = get_daily_specials(day)
-		print "%s Day %d" % (d["name"], day)
-		for c in d["specials"]:
-			print "  ", c
+		for r in get_daily_specials(day):
+			print "%s Day %d" % (r["name"], day)
+			for c in r["specials"]:
+				print "  ", c
+		print "-"*40
 
 if __name__ == "__main__":
 	main()
