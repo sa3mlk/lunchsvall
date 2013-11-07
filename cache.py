cf99c29fc2b6cd511dd99704e7b7809110c0c990
Jonas Gulle <jonas.gulle@gmail.com>
2013-11-07 09:26:22 +0100

    Fixed cache.py since abspath didn't work as I expected. Use another minifier from now on.

--- cache.py
+++ cache.py
@@ -8,7 +8,8 @@ def format_cache_file():
 	d = date.today()
 	cached_file = "cache/{y}/{m:02}/{d}.json".format(y=d.year, m=d.month, d=d)
 	cache_dir = os.path.dirname(cached_file)
-	return map(os.path.abspath, (cached_file, cache_dir))
+	base_path = os.path.dirname(os.path.realpath(__file__))
+	return [base_path + "/" + p for p in (cached_file, cache_dir)]
 
 if __name__ == "__main__":
 	f, d = format_cache_file()
