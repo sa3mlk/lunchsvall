1deab5aca7b707d61840104dbecba15091822638
Jonas Gulle <jonas.gulle@gmail.com>
2013-11-26 13:19:37 +0100

    Scraper is now working again after their redesign

--- scrapers/max.py
+++ scrapers/max.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-from BeautifulSoup import BeautifulSoup, NavigableString
+from BeautifulSoup import BeautifulSoup, Tag
 from urllib2 import urlopen
 from datetime import date
+import re
 
-URL = "http://www.max.se/maten.aspx"
+URL = "https://www.max.se/sv/Maten/Meny/Maltider/Dagens-Lunch/"
 
-def get_daily_specials():
+def get_daily_specials(day = None):
 	page = urlopen(URL)
 	soup = BeautifulSoup(page)
 	page.close()
@@ -20,27 +21,30 @@ def get_daily_specials():
 		"mapurl": "http://www.hitta.se/ViewDetailsPink.aspx?Vkiid=mFQRqbiq0V2C0M4wvtGY0w%253d%253d"
 	}
 
-	day = date.today().weekday()
+	day = date.today().weekday() if day is None else day
 
 	# Only Monday - Friday
 	if day > 4:
 		return daily_specials
 
-	h2 = soup.find("h2", text="Dagens lunch 10-14")
-	table = h2.parent.findNextSibling("table")
+	span = filter(lambda t: type(t) is Tag, soup.find("ul", {"class": "price"}).contents)[day].span.text
+	split = span.find(" - ")
+	if split == -1:
+		split = span.find("&nbsp;")
+		split += 6 if split > 0 else 0
+	else:
+		split += 3
+	split = 0 if split is -1 else split
+	daily_specials["specials"].append(span[split:].strip())
 
-	today = []
-	for td in table.findAll("td"):
-		today.append(filter(lambda x: isinstance(x, NavigableString), td.contents)[day])
-
-	daily_specials["specials"].append(unicode(today[1].replace(u"Ã¥", u"å")))
 	return daily_specials
 
 def main():
-	d = get_daily_specials()
-	print d["name"]
-	for c in d["specials"]:
-		print "  ", c
+	for d in range(5):
+		data = get_daily_specials(d)
+		print "Day {d}".format(d=d)
+		for c in data["specials"]:
+			print "  ", c
 
 if __name__ == "__main__":
 	main()
