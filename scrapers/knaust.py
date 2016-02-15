9ce1c50cf1ab409b4a459def4209d52f18871a42
Jonas Gulle <jonas.gulle@gmail.com>
2016-02-15 21:40:32 +0100

    Fixed scraper, closing issue #38

--- scrapers/knaust.py
+++ scrapers/knaust.py
@@ -28,20 +28,14 @@ def get_daily_specials(day=None):
 		return daily_specials
 
 	day = [u"Måndag", u"Tisdag", u"Onsdag", u"Torsdag", u"Fredag"][day]
-
-	search_items = [day, "Veckans fisk", "Veckans pasta", "Sallad"]
-	for i in search_items:
-		anchor = soup.find("strong", text=i)
-		daily_specials["specials"].extend(anchor.findAllNext(text=True, limit=1))
+	div = soup.find("div", {"class": "hidden-xs"}).find("strong", text=day)
+	daily_specials["specials"].extend(div.findAllNext(text=True, limit=3))
 
 	return daily_specials
 
 def main():
-	for day in range(5):
-		d = get_daily_specials(day)
-		print "%s Day %d" % (d["name"], day)
-		for c in d["specials"]:
-			print "  ", c
+	import test
+	test.run(get_daily_specials)
 
 if __name__ == "__main__":
 	main()
