92eed2493813d3819983deb57ad59668748e8592
Jonas Gulle <jonas.gulle@gmail.com>
2017-11-07 22:38:54 +0100

    Quickfix for Swedish characters

--- scrapers/tantanci.py
+++ scrapers/tantanci.py
@@ -27,25 +27,33 @@ def get_daily_specials(day=None):
 	if day == 5 or day == 6:
 		return daily_specials
 
-	days = [u"Måndag", u"Tisdag", u"Onsdag", u"Torsdag", u"Fredag"]
+	days = [u"ndag", u"Tisdag", u"Onsdag", u"Torsdag", u"Fredag"]
 	wday = days[day]
 	div = soup.find("div", {"class": "entry-content"})
-	p = filter(lambda t: t.text.find(wday) >= 0, div.findAll("p"))[0].text
+	try:
+		p = filter(lambda t: t.text.find(wday) >= 0, div.findAll("p"))[0].text
+	except IndexError:
+		return daily_specials
+
 	index = p.find(wday)
 	if day < 4:
 		daily_soup = p[index:p.find(days[day+1])]
-		daily_specials["specials"] = [daily_soup.replace(wday, "Dagens soppa").strip()]
+		daily_specials["specials"] = [daily_soup.replace(wday, "Dagens soppa")]
 	else:
 		daily_soup = p[index:]
-		daily_specials["specials"] = [daily_soup.replace(wday, "Dagens soppa").strip()]
+		daily_specials["specials"] = [daily_soup.replace(wday, "Dagens soppa")]
+
+	# Quick and dirty patch for Swedish characters.
+	def fix_string(s):
+		return s.replace(u"ĂĽ", u"å").replace(u"Ăś", u"ö").replace(u"Ă¤", u"ä").replace(u"Â", "").strip()
+
+	daily_specials["specials"] = [fix_string(x) for x in daily_specials["specials"]]
 
 	return daily_specials
 
 def main():
-	import simplejson as json
-	print get_daily_specials(1)["specials"][0].encode("utf8")
-	#import test
-	#test.run(get_daily_specials)
+	import test
+	test.run(get_daily_specials)
 
 if __name__ == "__main__":
 	main()
